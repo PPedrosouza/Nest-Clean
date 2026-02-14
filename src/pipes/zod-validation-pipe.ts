8b4a941084c7e152c05723b3cb9d5b5f4c37b8b3
Pedro Souza <pedro.olegario23@gmail.com>
2026-02-14 20:06:42 -0300

    ✨feat: zod validation and returns 2

--- src/pipes/zod-validation-pipe.ts
+++ src/pipes/zod-validation-pipe.ts
@@ -12,12 +12,14 @@ export class ZodValidationPipe implements PipeTransform {
     } catch (error) {
         if(error instanceof ZodError){
             throw new BadRequestException({
-                message: 'Validation failed!',
+                message: 'Validation failed',
                 statusCode: 400,
-                errors: fromZodError(error)
+                errors: fromZodError(error),
             })
         }
       throw new BadRequestException('Validation failed');
     }
+
+    return value
   }
 }
